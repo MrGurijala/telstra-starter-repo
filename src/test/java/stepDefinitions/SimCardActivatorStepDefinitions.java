56f73e6f5d66f2f683f7d3d7cc1cb7a226f87170
Mr.Gurijala <grvsaiteja@gmail.com>
2024-04-08 00:03:45 +0100

    added BDD task

--- src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
+++ src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
@@ -2,11 +2,16 @@ package stepDefinitions;
 
 import au.com.telstra.simcardactivator.SimCardActivator;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.junit.Assert;
+import org.springframework.http.ResponseEntity;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -14,5 +19,31 @@ import org.springframework.test.context.ContextConfiguration;
 public class SimCardActivatorStepDefinitions {
     @Autowired
     private TestRestTemplate restTemplate;
+
+    private String iccid;
+    private boolean activationStatus;
+
+    @Given("the ICCID {string} is submitted for activation")
+    public void the_iccid_is_submitted_for_activation(String iccid) {
+        this.iccid = iccid;
+    }
+
+    @When("the activation request is processed")
+    public void the_activation_request_is_processed() {
+        // Send activation request to the microservice
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                "http://localhost:8080/activate",
+                "{\"iccid\": \"" + iccid + "\", \"customerEmail\": \"test@example.com\"}",
+                String.class);
+
+        // Extract activation status from the response
+        activationStatus = response.getBody().contains("success");
+    }
+
+    @Then("the activation status should be {string}")
+    public void the_activation_status_should_be(String expectedStatus) {
+        boolean expectedActivationStatus = expectedStatus.equals("successful");
+        Assert.assertEquals(expectedActivationStatus, activationStatus);
+    }
 
 }
